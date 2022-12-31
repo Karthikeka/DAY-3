b98d4e0b3d233350158727a061bd2c930adf1c62
karthikeka <119919351+Karthikeka@users.noreply.github.com>
2022-12-31 21:23:24 +0530

    Add files via upload
    RESUME

---
+++ script.js
@@ -0,0 +1,52 @@
+let MYRESUME={
+    "Basics": {
+      "Name": "KARTHIK.E",
+      "email": "Karthikeka94@gamil.com",
+      "Phone": 9964269554,
+      "Degree": "B.E",
+      "location": {
+        "Address": "141/109 Sri krishna lyt hulimavu",
+        "PostalCode": 560076,
+        "City": "bangalore",
+        "State": "karnataka",
+        "Country": "India"
+      },
+      "profiles": [
+        {
+            "github":"https://github.com/karthikeka"
+        }
+      ]
+    },
+    
+    "education": [
+      {
+        "institution": "oxford College Of Engineering",
+        "department": "E&C",
+        "studyType": "fulltime",
+        "batch start year": 2018,
+        "batch end year": 2022,
+        "cgpa": 7,
+      }
+    ],
+    "Skills": [
+      {
+        "name": "javascript",
+        "level": "beginer",
+        
+          
+        
+      }
+    ],
+    "Languages": [
+      {
+        "language": "Tamil,English","kannada"
+      }
+    ],
+    "Interests": [
+      {
+        "Name": "script writter,youtuber,",
+      }
+    ]
+  }
+  console.log(MYRESUME);
+  
